3f6fa8cfe498465afd9b57418a4a19f1b95c0ab6
robmllze <86869208+robmllze@users.noreply.github.com>
2025-07-12 19:12:04 -0400

    update

--- lib/src/services/observed_polling_stream_service.dart
+++ lib/src/services/observed_polling_stream_service.dart
@@ -1,9 +1,10 @@
 //.title
 // ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
 //
-// Dart/Flutter (DF) Packages by dev-cetera.com & contributors. The use of this
-// source code is governed by an MIT-style license described in the LICENSE
-// file located in this project's root directory.
+// Copyright © dev-cetera.com & contributors.
+//
+// The use of this source code is governed by an MIT-style license described in
+// the LICENSE file located in this project's root directory.
 //
 // See: https://opensource.org/license/mit
 //
@@ -14,8 +15,7 @@ import '/_common.dart';
 
 // ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
 
-abstract class ObservedPollingStreamService<TData extends Object>
-    extends WidgetsBindingObserver
+abstract class ObservedPollingStreamService<TData extends Object> extends WidgetsBindingObserver
     with
         ServiceMixin,
         StreamServiceMixin<TData>,
--- lib/src/services/observed_stream_service.dart
+++ lib/src/services/observed_stream_service.dart
@@ -1,9 +1,10 @@
 //.title
 // ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
 //
-// Dart/Flutter (DF) Packages by dev-cetera.com & contributors. The use of this
-// source code is governed by an MIT-style license described in the LICENSE
-// file located in this project's root directory.
+// Copyright © dev-cetera.com & contributors.
+//
+// The use of this source code is governed by an MIT-style license described in
+// the LICENSE file located in this project's root directory.
 //
 // See: https://opensource.org/license/mit
 //
@@ -14,12 +15,8 @@ import '/_common.dart';
 
 // ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
 
-abstract class ObservedStreamService<TData extends Object>
-    extends WidgetsBindingObserver
-    with
-        ServiceMixin,
-        StreamServiceMixin<TData>,
-        HandleServiceLifecycleStateMixin {
+abstract class ObservedStreamService<TData extends Object> extends WidgetsBindingObserver
+    with ServiceMixin, StreamServiceMixin<TData>, HandleServiceLifecycleStateMixin {
   ObservedStreamService() {
     WidgetsBinding.instance.addObserver(this);
   }
